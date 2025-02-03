410511cdd6508e00cd107392aacc8513e928381b
DariusRadu <radu.va.darius@student.utcluj.ro>
2025-02-03 08:08:58 +0200

    Add .clang-format file to avoid sorting includes

--- Kara/src/Platform/Windows/WindowsWindow.cpp
+++ Kara/src/Platform/Windows/WindowsWindow.cpp
@@ -14,7 +14,7 @@ namespace {
 auto sGLFWInitialized{false};
 }
 
-WindowsWindow::WindowsWindow(const WindowProps& aProps)
+WindowsWindow::WindowsWindow(const WindowProps &aProps)
     : mData{aProps.mTitle, aProps.mSize} {
   KARA_CORE_INFO("Creating window {0}, {1}", mData.mTitle,
                  mData.mSize.ToString());
@@ -23,7 +23,7 @@ WindowsWindow::WindowsWindow(const WindowProps& aProps)
     auto success = glfwInit();
     KARA_CORE_ASSERT(success, "Can not initialize GLFW !");
 
-    glfwSetErrorCallback([](int aError, const char* aMessage) {
+    glfwSetErrorCallback([](int aError, const char *aMessage) {
       KARA_CORE_ERROR("GLFW Error: [{0}]: {1}", aError, aMessage);
     });
 
@@ -33,7 +33,7 @@ WindowsWindow::WindowsWindow(const WindowProps& aProps)
   mWindow = glfwCreateWindow(mData.mSize.mWidth, mData.mSize.mHeight,
                              mData.mTitle.c_str(), nullptr, nullptr);
   glfwMakeContextCurrent(mWindow);
-  glfwSetWindowUserPointer(mWindow, reinterpret_cast<void*>(&mData));
+  glfwSetWindowUserPointer(mWindow, reinterpret_cast<void *>(&mData));
   SetVSync(true);
 
   SetupEvents();
@@ -54,7 +54,7 @@ uint32 WindowsWindow::GetHeight() const { return mData.mSize.mHeight; }
 
 bool WindowsWindow::IsVSync() const { return mData.mVSync; }
 
-void WindowsWindow::SetEventCallback(const EventCallback& aCallback) {
+void WindowsWindow::SetEventCallback(const EventCallback &aCallback) {
   mData.mEventCallback = aCallback;
 }
 
@@ -69,9 +69,9 @@ void WindowsWindow::SetVSync(const bool aEnable) {
 
 void WindowsWindow::SetupEvents() {
   glfwSetWindowSizeCallback(
-      mWindow, [](GLFWwindow* aWindow, int aWidth, int aHeight) {
+      mWindow, [](GLFWwindow *aWindow, int aWidth, int aHeight) {
         auto data =
-            *reinterpret_cast<WindowData*>(glfwGetWindowUserPointer(aWindow));
+            *reinterpret_cast<WindowData *>(glfwGetWindowUserPointer(aWindow));
 
         EventSystem::WindowResizedEvent event{static_cast<float>(aWidth),
                                               static_cast<float>(aHeight)};
@@ -80,65 +80,65 @@ void WindowsWindow::SetupEvents() {
         data.mSize.mHeight = aHeight;
       });
 
-  glfwSetWindowCloseCallback(mWindow, [](GLFWwindow* aWindow) {
+  glfwSetWindowCloseCallback(mWindow, [](GLFWwindow *aWindow) {
     auto data =
-        *reinterpret_cast<WindowData*>(glfwGetWindowUserPointer(aWindow));
+        *reinterpret_cast<WindowData *>(glfwGetWindowUserPointer(aWindow));
 
     EventSystem::WindowClosedEvent event;
     data.mEventCallback(event);
   });
 
-  glfwSetKeyCallback(mWindow, [](GLFWwindow* aWindow, int aKey, int aScanCode,
+  glfwSetKeyCallback(mWindow, [](GLFWwindow *aWindow, int aKey, int aScanCode,
                                  int aAction, int aMods) {
     auto data =
-        *reinterpret_cast<WindowData*>(glfwGetWindowUserPointer(aWindow));
+        *reinterpret_cast<WindowData *>(glfwGetWindowUserPointer(aWindow));
 
     switch (aAction) {
-      case GLFW_PRESS: {
-        EventSystem::KeyPressedEvent e{aKey, 0};
-        data.mEventCallback(e);
-      } break;
+    case GLFW_PRESS: {
+      EventSystem::KeyPressedEvent e{aKey, 0};
+      data.mEventCallback(e);
+    } break;
 
-      case GLFW_RELEASE: {
-        EventSystem::KeyReleasedEvent e{aKey};
-        data.mEventCallback(e);
-      } break;
+    case GLFW_RELEASE: {
+      EventSystem::KeyReleasedEvent e{aKey};
+      data.mEventCallback(e);
+    } break;
 
-      case GLFW_REPEAT: {
-        EventSystem::KeyPressedEvent e{aKey, 1};
-        data.mEventCallback(e);
-      } break;
+    case GLFW_REPEAT: {
+      EventSystem::KeyPressedEvent e{aKey, 1};
+      data.mEventCallback(e);
+    } break;
 
-      default:
-        break;
+    default:
+      break;
     }
   });
 
   glfwSetMouseButtonCallback(
-      mWindow, [](GLFWwindow* aWindow, int aButton, int aAction, int aModes) {
+      mWindow, [](GLFWwindow *aWindow, int aButton, int aAction, int aModes) {
         auto data =
-            *reinterpret_cast<WindowData*>(glfwGetWindowUserPointer(aWindow));
+            *reinterpret_cast<WindowData *>(glfwGetWindowUserPointer(aWindow));
 
         switch (aAction) {
-          case GLFW_PRESS: {
-            EventSystem::MouseButtonPressedEvent e{aButton};
-            data.mEventCallback(e);
-          } break;
+        case GLFW_PRESS: {
+          EventSystem::MouseButtonPressedEvent e{aButton};
+          data.mEventCallback(e);
+        } break;
 
-          case GLFW_RELEASE: {
-            EventSystem::MouseButtonReleasedEvent e{aButton};
-            data.mEventCallback(e);
-          } break;
+        case GLFW_RELEASE: {
+          EventSystem::MouseButtonReleasedEvent e{aButton};
+          data.mEventCallback(e);
+        } break;
 
-          default:
-            break;
+        default:
+          break;
         }
       });
 
   glfwSetScrollCallback(
-      mWindow, [](GLFWwindow* aWindow, double aXOffset, double aYOffset) {
+      mWindow, [](GLFWwindow *aWindow, double aXOffset, double aYOffset) {
         auto data =
-            *reinterpret_cast<WindowData*>(glfwGetWindowUserPointer(aWindow));
+            *reinterpret_cast<WindowData *>(glfwGetWindowUserPointer(aWindow));
 
         EventSystem::MouseScrolledEvent e{static_cast<float>(aXOffset),
                                           static_cast<float>(aYOffset)};
@@ -146,9 +146,9 @@ void WindowsWindow::SetupEvents() {
       });
 
   glfwSetCursorPosCallback(
-      mWindow, [](GLFWwindow* aWindow, double aXPos, double aYPos) {
+      mWindow, [](GLFWwindow *aWindow, double aXPos, double aYPos) {
         auto data =
-            *reinterpret_cast<WindowData*>(glfwGetWindowUserPointer(aWindow));
+            *reinterpret_cast<WindowData *>(glfwGetWindowUserPointer(aWindow));
 
         EventSystem::MouseMovedEvent e{static_cast<float>(aXPos),
                                        static_cast<float>(aYPos)};
@@ -156,10 +156,10 @@ void WindowsWindow::SetupEvents() {
       });
 }
 
-}  // namespace Platform
+} // namespace Platform
 
-Window* Window::Create(const WindowProps& aProps) {
+Window *Window::Create(const WindowProps &aProps) {
   return new Platform::WindowsWindow(aProps);
 }
-}  // namespace Core
-}  // namespace Kara
+} // namespace Core
+} // namespace Kara
