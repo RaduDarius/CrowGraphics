f0086ec04f623f8dafa2b300e8d8ea789eb5f658
DariusRadu <radu.va.darius@student.utcluj.ro>
2025-03-09 19:22:17 +0200

    Change imgui includes to use relative paths

--- Kara/src/Kara/UI/UILayer.cpp
+++ Kara/src/Kara/UI/UILayer.cpp
@@ -10,8 +10,8 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 
-#include "imgui.h"
 #include <GLFW/glfw3.h>
+#include <imgui.h>
 
 namespace Kara {
 namespace UI {
